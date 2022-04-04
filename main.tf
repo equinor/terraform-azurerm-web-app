2f19fbd4a62682d65b39e8427e665b38aea381e8
Henrik Knutsen <henrik.knutsen@kleppnett.no>
2022-04-04 12:21:07 +0200

    feat: add azure ad authentication

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+locals {
+  default_app_settings = {
+    ACTIVE_DIRECTORY_AUTHENTICATION_SECRET = "@Microsoft.KeyVault(VaultName=${var.azuread_client_secret.vault_name};SecretName=${var.azuread_client_secret.secret_name})"
+  }
+}
+
 resource "azurerm_service_plan" "this" {
   name                = coalesce(var.app_service_plan_name, "plan-${var.app_name}-${var.environment_name}")
   resource_group_name = var.resource_group_name
@@ -14,13 +20,25 @@ resource "azurerm_linux_web_app" "this" {
 
   https_only = true
 
+  app_settings = merge(local.default_app_settings, var.app_settings)
+
   site_config {
     container_registry_use_managed_identity       = true
     container_registry_managed_identity_client_id = var.container_registry_managed_identity_client_id
   }
 
+  auth_settings {
+    enabled             = true
+    token_store_enabled = true
+
+    active_directory {
+      client_id                  = var.azuread_client_id
+      client_secret_setting_name = "ACTIVE_DIRECTORY_AUTHENTICATION_SECRET"
+    }
+  }
+
   identity {
-    type         = var.enable_system_assigned_managed_identity ? "SystemAssigned, UserAssigned" : "UserAssigned"
+    type         = "SystemAssigned, UserAssigned"
     identity_ids = var.managed_identity_ids
   }
 }
