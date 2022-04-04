cd067cddfc791118d172394c17ef6905c4a28967
Henrik Knutsen <henrik.knutsen@kleppnett.no>
2022-04-04 12:24:33 +0200

    chore: add comments to example

--- examples/webapp-with-container-registry/main.tf
+++ examples/webapp-with-container-registry/main.tf
@@ -9,11 +9,13 @@ locals {
 
 data "azurerm_client_config" "current" {}
 
+# Create a resource group
 resource "azurerm_resource_group" "example" {
   name     = "rg-${local.app_name}-${local.environment_name}"
   location = "northeurope"
 }
 
+# Create a log analytics workspace
 resource "azurerm_log_analytics_workspace" "example" {
   name                = "log-${local.app_name}-${local.environment_name}"
   location            = azurerm_resource_group.example.location
@@ -21,6 +23,7 @@ resource "azurerm_log_analytics_workspace" "example" {
   sku                 = "Free"
 }
 
+# Create a key vault
 module "vault" {
   source = "github.com/equinor/terraform-azurerm-vault?ref=3f7e3a44d1c366266c17561970924c60eb29058e"
 
@@ -37,12 +40,14 @@ module "vault" {
   }
 }
 
+# Create a key vault secret containing the client secret to be used for Azure AD authentication
 resource "azurerm_key_vault_secret" "example" {
   name         = "ClientSecret"
   value        = "super-secret-value"
   key_vault_id = module.vault.key_vault_id
 }
 
+# Create a container registry
 module "acr" {
   source = "github.com/equinor/terraform-azurerm-acr?ref=f426cb96b17e7e26faab096fdde61e390c6329bc"
 
@@ -54,6 +59,7 @@ module "acr" {
   managed_identity_operators = [data.azurerm_client_config.current.object_id]
 }
 
+# Create the web app
 module "webapp" {
   source = "../.."
 
@@ -62,9 +68,11 @@ module "webapp" {
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
 
+  # Assign identity with access to container registry to pull image from
   managed_identity_ids                          = [module.acr.managed_identity_id]
   container_registry_managed_identity_client_id = module.acr.managed_identity_client_id
 
+  # Configure Azure AD authentication
   azuread_client_id = "fe94e238-69a9-4633-94d0-c7f56dea76e8"
   azuread_client_secret = {
     vault_name  = module.vault.key_vault_name
@@ -72,6 +80,7 @@ module "webapp" {
   }
 }
 
+# Give web app access to key vault secret containing client secret
 resource "azurerm_key_vault_access_policy" "this" {
   key_vault_id = module.vault.key_vault_id
   tenant_id    = module.webapp.app_service_managed_identity.tenant_id
