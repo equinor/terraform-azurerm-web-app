2d606908b5c9641e692b4a2790e48acec659d5ea
Henrik Knutsen <henrik.knutsen@kleppnett.no>
2022-04-12 10:10:07 +0200

    Update test

---
+++ test/fixture/main.tf
@@ -0,0 +1,85 @@
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  application = random_id.this.hex
+  environment = "test"
+}
+
+resource "random_id" "this" {
+  byte_length = 8
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "this" {
+  name     = "rg-${local.application}-${local.environment}"
+  location = var.location
+}
+
+resource "azurerm_log_analytics_workspace" "this" {
+  name                = "log-${local.application}-${local.environment}"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  sku                 = "Free"
+}
+
+module "vault" {
+  source = "github.com/equinor/terraform-azurerm-vault?ref=4dc1ac5619bf309c452c336786aa6b0865ee99c3"
+
+  application = local.application
+  environment = local.environment
+
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  client_secret_permissions = ["Get", "List", "Set", "Delete", "Recover", "Backup", "Restore", "Purge"]
+
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.this.id
+}
+
+resource "azurerm_key_vault_secret" "this" {
+  name         = "ClientSecret"
+  value        = "my-client-secret"
+  key_vault_id = module.vault.key_vault_id
+}
+
+module "acr" {
+  source = "github.com/equinor/terraform-azurerm-acr?ref=8dd9c36f93173f5ee19857f5f789e0b7f0a7aa44"
+
+  application = local.application
+  environment = local.environment
+
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  managed_identity_operators = [data.azurerm_client_config.current.object_id]
+}
+
+module "web_app" {
+  source = "../.."
+
+  application = local.application
+  environment = local.environment
+
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  azuread_client_id = "fe94e238-69a9-4633-94d0-c7f56dea76e8"
+  azuread_client_secret = {
+    vault_name  = module.vault.key_vault_name
+    secret_name = azurerm_key_vault_secret.this.name
+  }
+
+  acr_identity_client_id = module.acr.user_assigned_identity_client_id
+  acr_identity_id        = module.acr.user_assigned_identity_id
+}
+
+resource "azurerm_key_vault_access_policy" "this" {
+  key_vault_id = module.vault.key_vault_id
+  tenant_id    = module.web_app.app_service_identity_tenant_id
+  object_id    = module.web_app.app_service_identity_principal_id
+
+  secret_permissions = ["Get"]
+}
