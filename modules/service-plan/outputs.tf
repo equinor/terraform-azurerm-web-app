db473f47564647fb60dc91b94273a6673b61659b
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-08-01 14:25:31 +0200

    Update outputs

--- modules/service-plan/outputs.tf
+++ modules/service-plan/outputs.tf
@@ -1,7 +1,9 @@
-output "name" {
-  value = azurerm_service_plan.this.name
+output "app_service_plan_name" {
+  description = "The name of this App Service plan."
+  value       = azurerm_service_plan.this.name
 }
 
-output "id" {
-  value = azurerm_service_plan.this.id
+output "app_service_plan_id" {
+  description = "The ID of this App Service plan."
+  value       = azurerm_service_plan.this.id
 }
