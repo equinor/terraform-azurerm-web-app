eecd73739f7538081bec8a229a64617926366f08
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-08-02 09:04:15 +0200

    Update service plan name logic

--- modules/service-plan/main.tf
+++ modules/service-plan/main.tf
@@ -1,11 +1,10 @@
 locals {
-  suffix       = "${var.application}-${var.environment}"
-  suffix_alnum = join("", regexall("[a-z0-9]", lower(local.suffix)))
-  tags         = merge({ application = var.application, environment = var.environment }, var.tags)
+  service_plan_name = substr(regex("^[a-zA-Z0-9-]+$", lower("plan-${var.application}-${var.environment}")), 0, 40)
+  tags              = merge({ application = var.application, environment = var.environment }, var.tags)
 }
 
 resource "azurerm_service_plan" "this" {
-  name                = "plan-${var.application}-${var.environment}"
+  name                = local.service_plan_name
   resource_group_name = var.resource_group_name
   location            = var.location
   os_type             = "Linux"
