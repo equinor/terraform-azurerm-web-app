47fafdce9682992c8490cc686e62e4ee72eb6383
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-09-21 15:04:53 +0200

    fix: correct ref to plan id

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "azurerm_linux_web_app" "this" {
   name                = var.app_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  service_plan_id     = var.service_plan_id
+  service_plan_id     = module.service_plan.id
 
   https_only = true
 
