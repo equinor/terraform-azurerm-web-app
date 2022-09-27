3a0db5f2a5e2ebec61d8217808641ee1d4dc3623
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-09-27 14:24:57 +0200

    fix: remove duplicate variable

--- variables.tf
+++ variables.tf
@@ -13,11 +13,6 @@ variable "location" {
   type        = string
 }
 
-variable "service_plan_name" {
-  description = "The name of this Web App service plan."
-  type        = string
-}
-
 variable "os_type" {
   description = "The OS type for the apps to be hosted on this Web App service plan."
   type        = string
