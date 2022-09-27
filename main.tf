91fce2f5aee095475f82f90bf6e4c1e3a8981d07
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-09-27 14:20:23 +0200

    feat: create windows apps

--- main.tf
+++ main.tf
@@ -4,15 +4,33 @@ module "service_plan" {
   name                = var.service_plan_name
   resource_group_name = var.resource_group_name
   location            = var.location
+  os_type             = var.os_type
   sku_name            = var.sku_name
 
   tags = var.tags
 }
 
 module "linux_app" {
-  for_each = var.apps
+  for_each = var.os_type == "Linux" ? var.apps : []
 
   source = "./modules/linux-app"
+
+  name                           = each.value.name
+  resource_group_name            = var.resource_group_name
+  location                       = var.location
+  service_plan_id                = module.service_plan.id
+  auth_settings_enabled          = each.value.auth_settings_enabled
+  aad_client_id                  = each.value.aad_client_id
+  aad_client_secret_setting_name = each.value.aad_client_secret_setting_name
+  acr_managed_identity_client_id = each.value.acr_managed_identity_client_id
+  managed_identity_ids           = each.value.managed_identity_ids
+  custom_hostnames               = each.value.custom_hostnames
+}
+
+module "windows_app" {
+  for_each = var.os_type == "Windows" ? var.apps : []
+
+  source = "./modules/windows-app"
 
   name                           = each.value.name
   resource_group_name            = var.resource_group_name
---
+++ modules/windows-app/README.md
@@ -0,0 +1,3 @@
+# Azure Windows Web App Terraform sub-module
+
+Terraform sub-module which creates an Azure Windows Web App.
