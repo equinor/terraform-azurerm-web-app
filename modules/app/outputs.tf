dfc43289795eff0a8a5cb5f09917203c4b7a73e7
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-01-12 12:52:03 +0100

    fix!: remove output

--- modules/app/outputs.tf
+++ modules/app/outputs.tf
@@ -8,11 +8,6 @@ output "name" {
   value       = local.web_app.name
 }
 
-output "aad_client_secret_setting_name" {
-  description = "The name of the app setting that should contain the client secret to use for Azure AD authentication."
-  value       = local.web_app.auth_settings[0].active_directory[0].client_secret_setting_name
-}
-
 output "identity_principal_id" {
   description = "The principal ID of the system-assigned identity of this Web App."
   value       = local.web_app.identity[0].principal_id
