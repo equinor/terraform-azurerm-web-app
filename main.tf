0ac6a7aafba3476f6c18f6dbfd609fc4ada4fbc4
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-01-12 09:31:43 +0100

    refactor!: combine app modules

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-locals {
-  apps = merge(module.windows_app, module.linux_app)
-}
-
 module "service_plan" {
   source = "./modules/service-plan"
 
@@ -14,34 +10,15 @@ module "service_plan" {
   tags = var.tags
 }
 
-module "linux_app" {
-  for_each = var.os_type == "Linux" ? var.apps : {}
+module "app" {
+  for_each = var.apps
 
-  source = "./modules/linux-app"
-
-  name                           = each.value.name
-  resource_group_name            = var.resource_group_name
-  location                       = var.location
-  service_plan_id                = module.service_plan.id
-  auth_settings_enabled          = each.value.auth_settings_enabled
-  aad_client_id                  = each.value.aad_client_id
-  aad_client_secret_setting_name = each.value.aad_client_secret_setting_name
-  websockets_enabled             = each.value.websockets_enabled
-  acr_managed_identity_client_id = each.value.acr_managed_identity_client_id
-  managed_identity_ids           = each.value.managed_identity_ids
-  custom_hostnames               = each.value.custom_hostnames
-  log_analytics_workspace_id     = var.log_analytics_workspace_id
-  tags                           = var.tags
-}
-
-module "windows_app" {
-  for_each = var.os_type == "Windows" ? var.apps : {}
-
-  source = "./modules/windows-app"
+  source = "./modules/app"
 
   name                           = each.value.name
   resource_group_name            = var.resource_group_name
   location                       = var.location
+  kind                           = var.os_type
   service_plan_id                = module.service_plan.id
   auth_settings_enabled          = each.value.auth_settings_enabled
   aad_client_id                  = each.value.aad_client_id
