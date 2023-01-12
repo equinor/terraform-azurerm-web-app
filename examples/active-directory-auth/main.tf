0ea0f5bef000dafec8f581271aa6009fc4ee0dd7
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-01-12 12:40:35 +0100

    chore: add example and test

---
+++ examples/active-directory-auth/main.tf
@@ -0,0 +1,42 @@
+provider "azurerm" {
+  features {
+    resource_group {
+      # Azure sometimes automatically creates a "default" app service plan that is not managed by Terraform,
+      # preventing Terraform from destroying the resource group.
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+resource "random_id" "this" {
+  byte_length = 8
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "this" {
+  name     = "rg-${random_id.this.hex}"
+  location = var.location
+}
+
+module "log_analytics" {
+  source = "github.com/equinor/terraform-azurerm-log-analytics?ref=v1.2.0"
+
+  workspace_name      = "log-${random_id.this.hex}"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+}
+
+module "web_app" {
+  source = "../.."
+
+  app_name                   = "app-${random_id.this.hex}"
+  resource_group_name        = azurerm_resource_group.this.name
+  location                   = azurerm_resource_group.this.location
+  service_plan_name          = "plan-${random_id.this.hex}"
+  log_analytics_workspace_id = module.log_analytics.workspace_id
+
+  auth_settings_active_directory = [{
+    client_id = "00000000-0000-0000-0000-000000000000"
+  }]
+}
