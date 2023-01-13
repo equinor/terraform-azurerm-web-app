f741a220daac309ac522260127feca76a52c7374
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-01-13 09:03:55 +0100

    fix: output null if identity not configured (#67)

--- modules/app/outputs.tf
+++ modules/app/outputs.tf
@@ -10,10 +10,10 @@ output "name" {
 
 output "identity_principal_id" {
   description = "The principal ID of the system-assigned identity of this Web App."
-  value       = var.identity != null ? local.web_app.identity[0].principal_id : null
+  value       = try(local.web_app.identity[0].principal_id, null)
 }
 
 output "identity_tenant_id" {
   description = "The tenant ID of the system-assigned identity of this Web App."
-  value       = var.identity != null ? local.web_app.identity[0].tenant_id : null
+  value       = try(local.web_app.identity[0].tenant_id, null)
 }
