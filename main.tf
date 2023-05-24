23316a312ce699960e1998897184bfaa972dc6ac
Sebastian Hana <43113824+sebastianlolv@users.noreply.github.com>
2023-05-24 09:12:05 +0200

    feat: add vnet route all enabled variable

--- main.tf
+++ main.tf
@@ -39,6 +39,7 @@ resource "azurerm_linux_web_app" "this" {
   }
 
   site_config {
+    vnet_route_all_enabled                        = var.vnet_route_all_enabled
     websockets_enabled                            = var.websockets_enabled
     container_registry_use_managed_identity       = var.container_registry_use_managed_identity
     container_registry_managed_identity_client_id = var.container_registry_managed_identity_client_id
@@ -89,7 +90,6 @@ resource "azurerm_windows_web_app" "this" {
   service_plan_id                 = var.app_service_plan_id
   key_vault_reference_identity_id = var.key_vault_reference_identity_id
   virtual_network_subnet_id       = var.virtual_network_subnet_id
-  vnet_route_all_enabled          = var.vnet_route_all_enabled
 
   # App settings should be configured during deployment.
   app_settings = null
@@ -114,6 +114,7 @@ resource "azurerm_windows_web_app" "this" {
   }
 
   site_config {
+    vnet_route_all_enabled                        = var.vnet_route_all_enabled
     websockets_enabled                            = var.websockets_enabled
     container_registry_use_managed_identity       = var.container_registry_use_managed_identity
     container_registry_managed_identity_client_id = var.container_registry_managed_identity_client_id
