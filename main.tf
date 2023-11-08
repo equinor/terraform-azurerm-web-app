1b6807580bf3e663f1e5eb5ce50de28c65b15c02
Sebastian Hana <43113824+sebastianlolv@users.noreply.github.com>
2023-11-08 10:03:15 +0100

    Update main.tf
    Co-authored-by: Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -140,7 +140,7 @@ resource "azurerm_windows_web_app" "this" {
     websockets_enabled                            = var.websockets_enabled
     container_registry_use_managed_identity       = var.container_registry_use_managed_identity
     container_registry_managed_identity_client_id = var.container_registry_managed_identity_client_id
-    ftps_state                                    = var.fpts_state
+    ftps_state                                    = var.ftps_state
   }
 
   dynamic "identity" {
