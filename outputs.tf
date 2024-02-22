310f2a4d78df0448f6e50c5e4529e630cb96f733
Stian Rossland <94676511+stiross79@users.noreply.github.com>
2024-02-22 13:29:44 +0100

    added default hostmane to output

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,8 @@ output "identity_tenant_id" {
   description = "The tenant ID of the system-assigned identity of this Web App."
   value       = try(local.web_app.identity[0].tenant_id, null)
 }
+
+output "default_hostname" {
+  description = "The default hostname of this Web App"
+  value       = local.web_app.default_hostname
+}
