0de5ce4e4bcf9ba2e1ee65f05f13b5f6818df85a
Stian Rossland <94676511+stiross79@users.noreply.github.com>
2024-02-22 08:30:40 +0100

    added dynamic blocks

--- main.tf
+++ main.tf
@@ -99,7 +99,15 @@ resource "azurerm_linux_web_app" "this" {
       }
     }
 
-    application_stack = var.application_stack
+    dynamic "application_stack" {
+      for_each = var.application_stack == null ? toset([]) : toset([var.application_stack])
+      content {
+        docker_image_name        = application_stack.value.docker_image_name
+        docker_registry_url      = application_stack.value.docker_registry_url
+        docker_registry_username = application_stack.value.docker_registry_username
+        docker_registry_password = application_stack.value.docker_registry_password
+      }
+    }
   }
 
   dynamic "identity" {
@@ -206,7 +214,16 @@ resource "azurerm_windows_web_app" "this" {
       }
     }
 
-    application_stack = var.application_stack
+    dynamic "application_stack" {
+      for_each = var.application_stack == null ? toset([]) : toset([var.application_stack])
+      content {
+        docker_image_name        = application_stack.value.docker_image_name
+        docker_registry_url      = application_stack.value.docker_registry_url
+        current_stack            = application_stack.value.current_stack
+        docker_registry_username = application_stack.value.docker_registry_username
+        docker_registry_password = application_stack.value.docker_registry_password
+      }
+    }
   }
 
   dynamic "identity" {
