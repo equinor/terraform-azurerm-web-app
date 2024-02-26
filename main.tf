8abf0c66c25e83c1d75c5afc2b426a95e6fd91ec
Stian Rossland <94676511+stiross79@users.noreply.github.com>
2024-02-26 13:24:35 +0100

    added variable and application stack

--- main.tf
+++ main.tf
@@ -101,8 +101,16 @@ resource "azurerm_linux_web_app" "this" {
       }
     }
 
-    # application_stack {}
-    # The arguments in this block is being set outside of Terraform during deployment.
+    dynamic "application_stack" {
+      for_each = var.application_stack == null ? toset([]) : toset([var.application_stack])
+
+      content {
+        docker_image_name        = application_stack.value.docker_image_name
+        docker_registry_url      = application_stack.value.docker_registry_url
+        docker_registry_username = application_stack.value.docker_registry_username
+        docker_registry_password = application_stack.value.docker_registry_password
+      }
+    }
   }
 
   dynamic "identity" {
@@ -209,8 +217,16 @@ resource "azurerm_windows_web_app" "this" {
       }
     }
 
-    # application_stack {}
-    # The arguments in this block is being set outside of Terraform during deployment.
+    dynamic "application_stack" {
+      for_each = var.application_stack == null ? toset([]) : toset([var.application_stack])
+
+      content {
+        docker_image_name        = application_stack.value.docker_image_name
+        docker_registry_url      = application_stack.value.docker_registry_url
+        docker_registry_username = application_stack.value.docker_registry_username
+        docker_registry_password = application_stack.value.docker_registry_password
+      }
+    }
   }
 
   dynamic "identity" {
