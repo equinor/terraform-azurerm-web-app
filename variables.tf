aaa0aaba438f13db6f6ff2a9e9f6f59fc15691ae
Sondre Sjølyst <sonyslyst@gmail.com>
2024-04-12 12:33:24 +0200

    Update variables.tf
    Co-authored-by: Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -79,8 +79,8 @@ variable "active_directory_client_secret_setting_name" {
 }
 
 variable "active_directory_login_parameters" {
-  description = "A map of key-value pairs to send to the Authorisation Endpoint when a user logs in."
-  type = map(string)
+  description = "A map of key-value pairs to send to the authorization endpoint when a user logs in."
+  type        = map(string)
   default     = {}
 }
 
