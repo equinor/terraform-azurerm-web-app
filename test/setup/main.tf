a77361da756654132312369ff6529994fe0f47ac
Sebastian Hana <43113824+sebastianlolv@users.noreply.github.com>
2024-08-15 10:27:51 +0200

    feat: implement terraform test functionality

--- test/setup/main.tf
+++ test/setup/main.tf
@@ -17,6 +17,6 @@ locals {
   name_suffix         = random_id.name_suffix.hex
   subscription_id     = random_uuid.subscription_id.result
   resource_group_name = "rg-${local.name_suffix}"
-  app_name            = "func-${local.name_suffix}"
+  app_name            = "app-${local.name_suffix}"
   app_service_plan_id = "/subscriptions/${random_uuid.subscription_id.result}/resourceGroups/${local.resource_group_name}/providers/Microsoft.Web/serverFarms/asp-${local.name_suffix}"
 }
--- test/setup/outputs.tf
+++ test/setup/outputs.tf
@@ -10,6 +10,18 @@ output "location" {
   value = "northeurope"
 }
 
+output "app_service_plan_id" {
+  value = local.app_service_plan_id
+}
+
+output "storage_account_name" {
+  value = "stfunc${local.name_suffix}"
+}
+
+output "storage_account_key" {
+  value = random_uuid.subscription_id.result
+}
+
 output "log_analytics_workspace_id" {
   value = "/subscriptions/${random_uuid.subscription_id.result}/resourceGroups/${local.resource_group_name}/providers/Microsoft.OperationalInsights/workspaces/log-${local.name_suffix}"
 }
