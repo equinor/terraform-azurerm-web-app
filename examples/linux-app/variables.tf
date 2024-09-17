36ffa02202f1c6b61a0459b333f21c3e57052258
Khan <akhan141@its.jnj.com>
2024-09-17 12:35:15 +0530

    customize feature for log&other parameters

--- examples/linux-app/variables.tf
+++ examples/linux-app/variables.tf
@@ -7,3 +7,10 @@ variable "location" {
   description = "The name of the location to create the resources in."
   type        = string
 }
+
+variable "logs_analytics_enable" {
+  description = "Should logs_analytics be enable for the web app?"
+  default = true
+  type = bool
+  
+}
