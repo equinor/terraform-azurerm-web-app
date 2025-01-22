0fc01ee15e89febf87e996a20c22622a54cd07d2
Sebastian Hana <43113824+sebastianlolv@users.noreply.github.com>
2025-01-22 11:36:09 +0100

    feat: create variable for virtual application path mappings

--- main.tf
+++ main.tf
@@ -258,6 +258,21 @@ resource "azurerm_windows_web_app" "this" {
         current_stack            = application_stack.value.current_stack
       }
     }
+
+    dynamic "virtual_application" {
+      for_each = var.virtual_application
+
+      content {
+        virtual_path    = virtual_application.value.virtual_path
+        physical_path   = virtual_application.value.physical_path
+        preload_enabled = virtual_application.value.preload_enabled
+
+        virtual_directory {
+          physical_path = virtual_application.value.virtual_directory.physical_path
+          virtual_path  = virtual_application.value.virtual_directory.virtual_path
+        }
+      }
+    }
   }
 
   dynamic "identity" {
