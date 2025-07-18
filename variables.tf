2f3f7a7f61cce1bf412aef547ba7489831ad88de
Prasanth2028 <prasanth.k2828@gmail.com>
2025-07-18 20:39:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -323,12 +323,6 @@ variable "diagnostic_setting_name" {
 variable "tags" {
   description = "A map of tags to assign to the resources."
   type        = map(string)
-  default     = {}
-
-  validation {
-    condition     = length(setintersection(["hidden-link: /app-insights-conn-string", "hidden-link: /app-insights-instrumentation-key", "hidden-link: /app-insights-resource-id"], keys(var.tags))) == 0
-    error_message = "Hidden tags (\"hidden-link: *\") are managed by Azure."
-  }
 }
 
 variable "storage_accounts" {
