b5ad555ed552162a8843398d0f127007f8920557
Prasanth2028 <prasanth.k2828@gmail.com>
2025-07-18 20:53:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -397,22 +397,6 @@ resource "azurerm_app_service_certificate_binding" "this" {
   ssl_state           = each.value["ssl_state"]
 }
 
-resource "azurerm_monitor_diagnostic_setting" "this" {
-  name                       = var.diagnostic_setting_name
-  target_resource_id         = local.web_app.id
-  log_analytics_workspace_id = var.log_analytics_workspace_id
-
-  # Ref: https://registry.terraform.io/providers/hashicorp/azurerm/3.65.0/docs/resources/monitor_diagnostic_setting#log_analytics_destination_type
-  log_analytics_destination_type = null
-
-  dynamic "enabled_log" {
-    for_each = toset(var.diagnostic_setting_enabled_log_categories)
-
-    content {
-      category = enabled_log.value
-    }
-  }
-
   dynamic "metric" {
     for_each = toset(concat(local.diagnostic_setting_metric_categories, var.diagnostic_setting_enabled_metric_categories))
 
